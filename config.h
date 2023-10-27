b61e385a0efa1d7bfd4a973ccbd4200c2905de73
Victor Fisac <victorfisac@gmail.com>
2023-10-27 23:12:10 +0200

    Implemeneted header file with some config defines

---
+++ config.h
@@ -0,0 +1,17 @@
+#if !defined(CONFIG_H)
+#define CONFIG_H
+
+// Project: Automatic plant watering
+// Technology: Arduino
+// Author: Victor Fisac (git@victorfisac)
+
+
+#define PROGRAM_SERIAL_PORT       9600
+#define PROGRAM_INTRO_DELAY       3000
+
+#define PROGRAM_LOOP_DELAY        100
+#define PROGRAM_DELTA_TIME        200
+
+#define WATER_LEVEL_SECURITY      0
+
+#endif
